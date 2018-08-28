8589cbb3df4c9a5c00640507978f0110f5015bde
keartie94 <keartie94@gmail.com>
2018-08-28 19:14:35 +0200

    Calculator test

--- test/CalculatorTest.php
+++ test/CalculatorTest.php
@@ -2,7 +2,7 @@
 
 require "vendor/autoload.php";
 
-class CalculatorTest extends PHPUnit_Framework_TestCase{
+class CalculatorTest extends PHPUnit\Framework\TestCase\{
   public function testAddNumbers(){
     $calc=new Calculator;
     $this->assertEquals(4,$calc->add(2,2));
