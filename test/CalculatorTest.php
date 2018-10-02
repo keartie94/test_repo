d44384679bd5b7c7d2a61c9b7614cba35f3bbbf5
keartie94 <keartie94@gmail.com>
2018-10-02 19:29:09 +0200

    aaaa

--- test/CalculatorTest.php
+++ test/CalculatorTest.php
@@ -14,7 +14,7 @@ class CalculatorTest extends TestCase{
     $calc=new Calculator;
     $this->assertEquals(3,$calc->subtract(5,2));
   }*/
-  
+
   public function testDivide(){
     $calc=new Calculator;
     $this->assertEquals(3,$calc->divide(12,4));
@@ -36,6 +36,19 @@ class CalculatorTest extends TestCase{
 
         // $stub->doSomething('foo') returns 'foo'
         $this->assertEquals(5, $stub->subtract(7,2));
+    }
+
+    public function dbTest(){
+      $mysql_host = getenv('MYSQL_HOST') ?: 'localhost';
+      $mysql_user = getenv('MYSQL_USER') ?: 'root';
+      $mysql_password = getenv('MYSQL_PASSWORD') ?: '';
+      $connection_string = "mysql:host={$mysql_host};dbname=moodle";
+      $db = new PDO($connection_string, $mysql_user, $mysql_password);
+
+      $stmt = $db->prepare("SELECT id FROM mdl_assignfeedback_witsoj WHERE id=1");
+      $stmt->execute();
+      $result = $stmt->fetchAll();
+      $this->assertEquals(1,$result,"Yay!");
     }
 }
 
